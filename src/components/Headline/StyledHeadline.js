0b133e24c3b14f26cdda6cd49abf51cfb6a7d2cf
C K <cl-ki@web.de>
2022-08-15 14:21:23 +0200

    add initial styles

---
+++ src/components/Headline/StyledHeadline.js
@@ -0,0 +1,9 @@
+import StyledHeadlineVariants from './StyledHeadlineVariants';
+
+export default function StyledHeadline1() {
+	return (
+		<>
+			<StyledHeadlineVariants variant="h1">Great to have you here</StyledHeadlineVariants>
+		</>
+	);
+}
--- src/styles.js
+++ src/styles.js
@@ -14,5 +14,16 @@ export const GlobalStyle = createGlobalStyle`
 	body {
 		margin: 0;
 		font-size: 1rem;
+
 	}
+
+:root {
+--turq: #006D77;
+--turq_light: #83C5BE;
+--neutral: #edf6f9;
+--rose: #ffddd2;
+--salmon: #e29578;
+}
+
+
 `;
