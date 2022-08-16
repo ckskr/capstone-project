967e2c94b43941368dc38b230dff636a2431081c
C K <cl-ki@web.de>
2022-08-16 14:23:43 +0200

    add submit functionality for each input and show in list

--- src/components/Form/StyledForm.js
+++ src/components/Form/StyledForm.js
@@ -1,4 +1,3 @@
-import {nanoid} from 'nanoid';
 import {useState} from 'react';
 import styled from 'styled-components';
 
@@ -19,11 +18,25 @@ const StyledLabel = styled.label`
 	display: none;
 `;
 
+/*function handleSubmit(event) {
+	event.preventDefault();
+	const form = event.target;
+	const {name, date} = form.elements;
+	setName(name.value);
+	setDate(date.value);
+}
+onSubmit={handleSubmit}
+*/
+
 export default function StyledForm() {
 	const [inputValue, setInputValue] = useState('');
+	const [inputValue1, setInputValue1] = useState('');
+	const [inputValue2, setInputValue2] = useState('');
 
 	const entries = useStore(state => state.entries);
 	const addEntry = useStore(state => state.addEntry);
+	const addEntry1 = useStore(state => state.addEntry);
+	const addEntry2 = useStore(state => state.addEntry);
 
 	return (
 		<>
@@ -32,12 +45,18 @@ export default function StyledForm() {
 				onSubmit={event => {
 					event.preventDefault();
 					addEntry(inputValue);
+					addEntry1(inputValue1);
+					addEntry2(inputValue2);
 					setInputValue('');
+					setInputValue1('');
+					setInputValue2('');
 				}}
 			>
-				<StyledLabel htmlFor="GratitudeDiary">Form for GratitudeJournal </StyledLabel>
+				<StyledLabel htmlFor="GratitudeDiary">Form for GratitudeDiary </StyledLabel>
 				<StyledTextArea
-					id={nanoid}
+					aria-label="Form for GratitudeDiary"
+					id="GratitudeJournal1"
+					name="GratitudeDiary1"
 					placeholder="Name a thing you are grateful for today"
 					type="text"
 					required
@@ -48,25 +67,29 @@ export default function StyledForm() {
 					}}
 				/>
 				<StyledTextArea
-					id={nanoid}
-					placeholder="Surely there is another thing you are grateful for"
+					aria-label="Form for GratitudeDiary"
+					id="GratitudeJournal2"
+					name="GratitudeDiary2"
+					placeholder="Surely there was another great thing happening today"
 					type="text"
 					required
 					minLength="3"
-					value={inputValue}
+					value={inputValue1}
 					onChange={event => {
-						setInputValue(event.target.value);
+						setInputValue1(event.target.value);
 					}}
 				/>
 				<StyledTextArea
-					id={nanoid}
-					placeholder="Think back to a moment that made you happy today"
+					aria-label="Form for GratitudeDiary"
+					id="GratitudeJournal3"
+					name="GratitudeDiary3"
+					placeholder="Think harder about a last thing that you are grateful for"
 					type="text"
 					required
 					minLength="3"
-					value={inputValue}
+					value={inputValue2}
 					onChange={event => {
-						setInputValue(event.target.value);
+						setInputValue2(event.target.value);
 					}}
 				/>
 				<StyledButton type="submit">Add to my diary</StyledButton>
