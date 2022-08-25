55be5faaa38535899c19a4d6895a19a7f66a9cca
C K <cl-ki@web.de>
2022-08-25 11:05:02 +0200

    add changes required by vercel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import FormExport from '../components/Form/Form';
+import Form from '../components/Form/Form';
 import Headline1 from '../components/Headline/Headline1';
 import Navigation from '../components/Navigation/Navigation';
 
@@ -6,7 +6,7 @@ export default function HomePage() {
 	return (
 		<>
 			<Headline1 />
-			<FormExport />
+			<Form />
 			<Navigation />
 		</>
 	);
