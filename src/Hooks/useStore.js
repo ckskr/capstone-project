3a804528218ace41607ac953fe46881f6eb223a5
C K <cl-ki@web.de>
2022-08-31 13:23:18 +0200

    add persist_its working

--- src/Hooks/useStore.js
+++ src/Hooks/useStore.js
@@ -1,17 +1,26 @@
 import create from 'zustand';
+import {persist} from 'zustand/middleware';
 
-const useStore = create(set => ({
-	entries: [],
-	addEntry: entry => {
-		set(state => {
-			return {entries: [...state.entries, entry]};
-		});
-	},
+const useStore = create(
+	persist(
+		set => ({
+			entries: [],
+			addEntry: entry => {
+				set(state => {
+					return {entries: [...state.entries, entry]};
+				});
+			},
 
-	deleteEntry: id => {
-		set(state => {
-			return {entries: state.entries.filter(entry => entry.id !== id)};
-		});
-	},
-}));
+			deleteEntry: id => {
+				set(state => {
+					return {entries: state.entries.filter(entry => entry.id !== id)};
+				});
+			},
+		}),
+		{
+			name: 'Entries', // unique name
+			getStorage: () => localStorage,
+		}
+	)
+);
 export default useStore;
