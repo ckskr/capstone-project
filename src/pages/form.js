c5ef8c54bd0c27460e5e294e416eaad051a47ae6
C K <cl-ki@web.de>
2022-08-31 16:03:16 +0200

    add welcome page

---
+++ src/pages/form.js
@@ -0,0 +1,13 @@
+import Form from '../components/Form/Form';
+import Headline1 from '../components/Headline/Headline1';
+import Navigation from '../components/Navigation/Navigation';
+
+export default function HomePage() {
+	return (
+		<>
+			<Headline1 />
+			<Form />
+			<Navigation />
+		</>
+	);
+}
