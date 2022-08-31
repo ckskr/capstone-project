dd59d680d3545164e7effb0fa851976fde60532d
C K <cl-ki@web.de>
2022-08-31 16:03:16 +0200

    add welcome page

--- src/Hooks/useStore.js
+++ src/Hooks/useStore.js
@@ -1,26 +1,17 @@
 import create from 'zustand';
-import {persist} from 'zustand/middleware';
 
-const useStore = create(
-	persist(
-		set => ({
-			entries: [],
-			addEntry: entry => {
-				set(state => {
-					return {entries: [...state.entries, entry]};
-				});
-			},
+const useStore = create(set => ({
+	entries: [],
+	addEntry: entry => {
+		set(state => {
+			return {entries: [...state.entries, entry]};
+		});
+	},
 
-			deleteEntry: id => {
-				set(state => {
-					return {entries: state.entries.filter(entry => entry.id !== id)};
-				});
-			},
-		}),
-		{
-			name: 'Entries', // unique name
-			getStorage: () => localStorage,
-		}
-	)
-);
+	deleteEntry: id => {
+		set(state => {
+			return {entries: state.entries.filter(entry => entry.id !== id)};
+		});
+	},
+}));
 export default useStore;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,66 @@
-import Form from '../components/Form/Form';
-import Headline1 from '../components/Headline/Headline1';
-import Navigation from '../components/Navigation/Navigation';
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
+import {useRouter} from 'next/router';
+import styled from 'styled-components';
 
-export default function HomePage() {
+export default function StartPage() {
+	const DynamicWrapper = dynamic(() => import('../components/styledWrapper'), {
+		ssr: false,
+	});
+	const router = useRouter();
 	return (
-		<>
-			<Headline1 />
-			<Form />
-			<Navigation />
-		</>
+		<DynamicWrapper>
+			<StyledH1>
+				Welcome to Daimoo
+				<div>your daily mood check</div>
+				<h2>Great to have you here</h2>
+			</StyledH1>
+			<StyledBox>
+				<Link href="./form">
+					<StyledLink active={router.pathname === '/index'}>
+						How did you feel today?
+					</StyledLink>
+				</Link>
+
+				<Link href="./diary">
+					<StyledLink active={router.pathname === '/diary'}>
+						Your mood in a retrospective
+					</StyledLink>
+				</Link>
+			</StyledBox>
+		</DynamicWrapper>
 	);
 }
+
+const StyledH1 = styled.h1`
+	color: var(--turq);
+	& div {
+		color: var(--turq);
+		font-size: 20px;
+	}
+	& h2 {
+		color: var(--turq_light);
+		font-size: 20px;
+	}
+`;
+
+const StyledLink = styled.a`
+	display: flex;
+	flex-wrap: wrap;
+	width: 120px;
+	height: 70px;
+	margin: 10px;
+	border-radius: 5px;
+	background-color: var(--neutral);
+	box-shadow: rgba(17, 17, 26, 0.05) 0 1px 0, rgba(17, 17, 26, 0.1) 0 0 8px;
+	color: var(--salmon);
+
+	&:hover {
+		font-weight: 1000;
+	}
+`;
+
+const StyledBox = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+`;
