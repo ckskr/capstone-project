42cdb42b08d2a22f4c629ec18096971a043e49e8
C K <cl-ki@web.de>
2022-09-01 14:17:49 +0200

    add time in entries

--- src/pages/diary.js
+++ src/pages/diary.js
@@ -22,6 +22,7 @@ export default function Diary() {
 					entries.map(entry => {
 						return (
 							<StyledCard key={entry.id}>
+								<StyledDate>{entry.date}</StyledDate>
 								<StyledFeeling>That is how you felt:</StyledFeeling>
 								<ul>
 									<StyledLi>{entry.mood}</StyledLi>
@@ -94,4 +95,12 @@ const StyledP = styled.p`
 		color: var(--turq_light);
 		font-weight: bold;
 	}
+`;
+
+const StyledDate = styled.div`
+	position: absolute;
+	top: 5px;
+	right: 15px;
+	color: var(--turq_light);
+	font-size: 12px;
 `;
